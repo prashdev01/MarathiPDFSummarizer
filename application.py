ee1b99bc8edf12485f785a4716896e1ff8ad650c
Prashant wadhave <125750774+prashdev01@users.noreply.github.com>
2024-03-07 14:43:53 +0530

    Front-end completed

--- application.py
+++ application.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask,render_template ,request ,send_file 
+import pymongo
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 
-if __name__ == '__main__':
+client =pymongo.MongoClient("mongodb+srv://prashantwadhave5:Prash2002@cluster0.wkfocs3.mongodb.net/?retryWrites=true&w=majority") # connecting string for mongodb database for the contacted data
+db = client.test
+db = client.PDfSummriser  
+collection = db['contact_list'] 
+
+
+@app.route('/')
+def index():
+    return render_template('index.html') # for rendering the index html file
+
+@app.route('/submit_pdf', methods=['POST']) # function to process on the pdf
+def submit_pdf():
+    pass
+
+@app.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        print(f"Name: {name}, Email: {email}, Message: {message}")
+
+        # Store the form data in MongoDB
+        collection.insert_one({
+            'name': name,
+            'email': email,
+            'message': message
+        })
+
+        return  render_template('formsub.html')
+    else:
+        return "Method Not Allowed", 405
+
+
+    
+if __name__ == ('__main__'):
     app.run(debug=True)
